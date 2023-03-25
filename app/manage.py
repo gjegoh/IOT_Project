eb9f63d88bf3780efd8069e523e14651e08a11f6
Aloysius <chewinggum222@hotmail.com>
2023-03-25 20:29:33 +0800

    added simple datetimepicker, created container for tele reminder bot
    datetimepicker too much dependencies to style
    needed separate container for tele bot because of jobqueue

--- app/manage.py
+++ app/manage.py
@@ -2,11 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import datetime
-from telegram.ext import Updater
 
-def daily_reminder(context):
-    context.bot.send_message(chat_id=os.environ.get("CHAT_ID"), text='This is your daily reminder to submit your CBG reading if you have not!   ')
+
 
 def main():
     """Run administrative tasks."""
@@ -21,15 +18,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-    #Send daily message to the user who's subscribed to DiaBreezyBot
-    TOKEN = os.environ.get("TELE_TOKEN")
-    updater = Updater(TOKEN, use_context=True)
-    job = updater.job_queue
-    timeToSendMsg = datetime.time(14, 50, 00, 000000) #24hr clock, uses UTC timing = SG time -8. For example, 10pm SGT = 2pm UTC = 1400H GMT
-    job.run_daily(daily_reminder, timeToSendMsg, days=(0, 1, 2, 3, 4, 5, 6), context=None, name=None)
-    updater.start_polling()
-    updater.idle()
 
 
 if __name__ == '__main__':
---
+++ tele_bot/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Start server
+echo "###   Starting telegram bot listener   ###"
+python telebot.py runserver 0.0.0.0:8001
