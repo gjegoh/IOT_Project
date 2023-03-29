ad2457a325e409ddd0f8cc7ded5a4fe4d9ad809c
junyiseng <junyi97@gmail.com>
2023-03-29 21:24:03 +0800

    updated dashboard
    Added Weekly Food Nutrients Intake graph, Diabreezy Logbook, Summary of the day

--- app/Main/views.py
+++ app/Main/views.py
@@ -7,30 +7,74 @@ from CBG.models import CBG_Food_Record
 import datetime
 from pytz import timezone
 
+# # endpoint to render index page for all users
+# def index(request):
+#     if request.method == 'GET':
+#         query_set = CBG_Food_Record.objects.all().order_by('Before_CBG_Uploaded_At__minute')
+#         readings = []
+#         measurements = []
+#         foodnames = []
+#         cbg_datetime = []
+#         for i in query_set:
+#             readings.append(i.Before_CBG_Reading)
+#             readings.append(i.After_CBG_Reading)
+#             measurements.append(i.Before_CBG_Measurement)
+#             measurements.append(i.After_CBG_Measurement)
+#             foodnames.append(i.Food_Name)
+#             cbg_datetime.append(i.Before_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"))
+#             cbg_datetime.append(i.After_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"))
+#         return render(
+#             request,
+#             'Main/index.html',
+#             context={
+#                 'readings': readings,
+#                 'measurements': measurements,
+#                 'foodnames': foodnames,
+#                 'cbg_datetime': cbg_datetime
+#             }
+#         )
+
 # endpoint to render index page for all users
 def index(request):
     if request.method == 'GET':
         query_set = CBG_Food_Record.objects.all().order_by('Before_CBG_Uploaded_At__minute')
-        readings = []
-        measurements = []
-        foodnames = []
-        cbg_datetime = []
+        results = []
+        food = []
+        foodlist= []
+
         for i in query_set:
-            readings.append(i.Before_CBG_Reading)
-            readings.append(i.After_CBG_Reading)
-            measurements.append(i.Before_CBG_Measurement)
-            measurements.append(i.After_CBG_Measurement)
-            foodnames.append(i.Food_Name)
-            cbg_datetime.append(i.Before_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"))
-            cbg_datetime.append(i.After_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"))
+
+            each_entry = {
+                'Before_CBG_Reading': i.Before_CBG_Reading, 
+                'After_CBG_Reading': i.After_CBG_Reading,
+                'Before_CBG_Measurement': i.Before_CBG_Measurement,
+                'After_CBG_Measurement': i.After_CBG_Measurement,
+                'Food_Name': i.Food_Name,
+                'Food_Uploaded_At': i.Food_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"),
+                'Before_CBG_Uploaded_At': i.Before_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M"),
+                'After_CBG_Uploaded_At': i.After_CBG_Uploaded_At.astimezone(timezone('Asia/Singapore')).strftime("%Y/%m/%d, %H:%M")
+            }
+            results.append(each_entry)
+
+
+            if i.Food_Name not in foodlist:
+                print(i.Food_Name)
+                foodlist.append(i.Food_Name)
+                each_food = {
+                    'Food_Name': i.Food_Name,
+                    'Food_Calorie': i.Food_Calorie,
+                    'Food_Carb': i.Food_Carb,
+                    'Food_Sugar': i.Food_Sugar,
+                    'Food_Fibre': i.Food_Fibre
+                }
+                food.append(each_food)
+             
         return render(
             request,
             'Main/index.html',
             context={
-                'readings': readings,
-                'measurements': measurements,
-                'foodnames': foodnames,
-                'cbg_datetime': cbg_datetime
+                'food': food,
+                'results': results
             }
         )
 
